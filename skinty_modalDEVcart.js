3350ab36cb1da0d14b09e7a4e30fd0cfe442352e
skintifique <78672198+skintifique@users.noreply.github.com>
2021-02-13 14:44:01 +0100

    Update skinty_modalDEVcart.js

--- skinty_modalDEVcart.js
+++ skinty_modalDEVcart.js
@@ -69,8 +69,7 @@ allConditions = "0" ;
 }
 
 function selectSkintyExit () {
-  let urlForConditions3 = window.location.href ;
-  let testOnOrderPage = urlForConditions3.includes("multi-shipping") ;
+  let testOnOrderPage = urlForConditions.includes("multi-shipping") ;
   document.getElementById("dev").innerHTML = testOnOrderPage ;
   if (testOnOrderPage) {
     exitRef = document.getElementById("exitWithCartTheme") ;
