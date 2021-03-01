67cbd4432ccc481086b4e2adcff5a54f539c1b9b
skintifique <78672198+skintifique@users.noreply.github.com>
2021-03-01 22:32:12 +0100

    Update skinty_205_prod.js

--- skinty_205_prod.js
+++ skinty_205_prod.js
@@ -20,8 +20,8 @@
 
 // SET UP SELECT PAGE PARAMETERS
 // Image in help center float btn
-document.getElementById("helpPopupBtnImg").src = "https://media.giphy.com/media/SUbYK8slyVWbmHc5R9/giphy.gif" ;
-// potential alternative gif: https://media.giphy.com/media/26ufn24Onjz8w7NxS/giphy.gif
+document.getElementById("helpPopupBtnImg").src = "https://media.giphy.com/media/26ufn24Onjz8w7NxS/giphy.gif" ;
+// potential alternative gif: https://media.giphy.com/media/SUbYK8slyVWbmHc5R9/giphy.gif
 // url for skintypage
 let skintyPageUrl= "https://skintifique.github.io" ;
 
