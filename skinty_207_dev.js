7a7870ed50b3101585e98f8ee0de36939fd6b882
skintifique <78672198+skintifique@users.noreply.github.com>
2021-03-28 03:56:09 +0200

    Update skinty_207_dev.js

--- skinty_207_dev.js
+++ skinty_207_dev.js
@@ -506,16 +506,6 @@ document.getElementById("skintyframe").src = skintyPageUrl + "/call_us.html" ;
 //  </script>
 	
   function openChat() {
-let script = document.createElement("script");
-script.setAttribute("type", "text/javascript");
-script.setAttribute("id", "ze-snippet");
-script.setAttribute("src", "https://static.zdassets.com/ekr/snippet.js?key=a11224ff-78c1-49da-8419-1a8006595ef5");
-let divZendesk = document.getElementById("divZendesk") ;
-divZendesk.appendChild(script);	
-openChat2 () ;
-}
-	
-function openChat2() {
   let current_url = window.location.href ;	  
   let lang ;
   if (current_url.includes("/fr/")) {
