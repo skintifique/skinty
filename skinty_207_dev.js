885308fc4b9c6e16dd3f542b0731ca753faa11bf
skintifique <78672198+skintifique@users.noreply.github.com>
2021-03-28 04:36:45 +0200

    Update skinty_207_dev.js

--- skinty_207_dev.js
+++ skinty_207_dev.js
@@ -239,6 +239,9 @@ script.setAttribute("id", "ze-snippet");
 script.setAttribute("src", "https://static.zdassets.com/ekr/snippet.js?key=a11224ff-78c1-49da-8419-1a8006595ef5");
 let pZendesk = document.getElementById("pZendesk") ;
 pZendesk.appendChild(script);
+	
+zE("webWidget", "setLocale", "en_us");
+zE("webWidget", "hide");
 
 }
 <!-- END display floating button and identification deviceType -->  
