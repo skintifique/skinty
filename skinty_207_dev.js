44db0b1189072ef38278cd10224c50df8e5978dd
skintifique <78672198+skintifique@users.noreply.github.com>
2021-03-28 05:30:18 +0200

    Update skinty_207_dev.js

--- skinty_207_dev.js
+++ skinty_207_dev.js
@@ -240,6 +240,11 @@ script1.setAttribute("src", "https://static.zdassets.com/ekr/snippet.js?key=a112
 let pZendesk = document.getElementById("pZendesk") ;
 pZendesk.appendChild(script1);
  })();
+	
+(function() {
+  zE("webWidget", "hide");
+  document.getElementById("myPopupDiv").style.display = "block";
+})();	  
 
 }
 	
