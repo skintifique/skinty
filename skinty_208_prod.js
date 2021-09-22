022f5dc683861bea3c69ed549885ec7fedb2c1fa
skintifique <78672198+skintifique@users.noreply.github.com>
2021-09-22 22:03:30 +0200

    Update skinty_208_prod.js

--- skinty_208_prod.js
+++ skinty_208_prod.js
@@ -53,6 +53,8 @@ show_digital_assistant = document.getElementById("show_digital_assistantFR").val
 welcomeTextHelpPopup = document.getElementById("text_for_welcomeTextHelpPopupEN").value ;
 show_on_exit = document.getElementById("show_on_exitEN").value ;
 show_digital_assistant = document.getElementById("show_digital_assistantEN").value ;
+document.getElementById("email_link_popup").value = "https://www.skintifique.me/fr/contactez-nous/" ;
+document.getElementById("email_link_modal").value = "https://www.skintifique.me/fr/contactez-nous/" ;
 }	
 document.getElementById("welcomeTextHelpPopup").innerHTML = welcomeTextHelpPopup ;
 if (show_digital_assistant == "no") {
