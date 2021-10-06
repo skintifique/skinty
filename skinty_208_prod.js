034ef6f6e254ea9a27abfb8ce061ae4315605a14
skintifique <78672198+skintifique@users.noreply.github.com>
2021-10-06 22:21:33 +0200

    Update skinty_208_prod.js

--- skinty_208_prod.js
+++ skinty_208_prod.js
@@ -626,7 +626,8 @@ if (offsetRect < 40 ){
 let additional_page_style = document.getElementById("additional-page-style").value ;
 if (additional_page_style) {	
 document.getElementById("dev1").innerHTML = additional_page_style ;
-body.classList.add(additional_page_style);
+let wrapper_elmt = document.getElementById("wrapper") ;
+wrapper_elmt.classList.add(additional_page_style);
 }
  
 <!-- End script to add additional style to specific pages -->
