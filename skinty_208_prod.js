48226e98bb1441a3a442726385bc16175a08351d
skintifique <78672198+skintifique@users.noreply.github.com>
2021-10-09 01:50:47 +0200

    Update skinty_208_prod.js

--- skinty_208_prod.js
+++ skinty_208_prod.js
@@ -8,7 +8,7 @@ function loadMenuImages () {
 document.getElementById("dev1XX").innerHTML = "12" ;
 
  let imgMenu1 = document.getElementById("imgMenu1").getAttribute("data-menu-src") ;
- document.getElementById("img-menu1").src = imgMenu1 ;	
+ document.getElementById("imgMenu1").src = imgMenu1 ;	
 }
 <!-- End script to load top menu images (desktop) only when user needs them -->
 
