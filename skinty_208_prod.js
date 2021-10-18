3ddbaa1499cc039a9da27c39e87785035f564eaf
skintifique <78672198+skintifique@users.noreply.github.com>
2021-10-19 00:34:50 +0200

    Update skinty_208_prod.js

--- skinty_208_prod.js
+++ skinty_208_prod.js
@@ -107,7 +107,7 @@ document.getElementById("ModalBtnFloat").style.display = "none";
 // END SCRIPT TO ADD BANNER TEXT BEFORE PRICE DISCOUNT TABLE ON PRODUCT PAGE 
 
 //  SCRIPT TO MAKE FIRST PAYMENT OPTION VISIBLE AND CORRESPONDING RADIO BTN CKECKED ON PAYMENT PAGE
-if (pageUrl.includes("/en/order")) {
+if ((pageUrl.includes("/en/order")) || (pageUrl.includes("/fr/commande")))  {
 let payment_option_1 = document.getElementById("payment-option-1") ;
 let pay_with_payment_option_1_form = document.getElementById("pay-with-payment-option-1-form") ;
 payment_option_1.checked = "checked" ;
