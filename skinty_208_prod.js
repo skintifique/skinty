d72f89f135297f21629c0ded81a0ba5cf4f939e1
skintifique <78672198+skintifique@users.noreply.github.com>
2021-10-18 23:32:10 +0200

    Update skinty_208_prod.js

--- skinty_208_prod.js
+++ skinty_208_prod.js
@@ -110,7 +110,7 @@ document.getElementById("ModalBtnFloat").style.display = "none";
 let payment_option_1 = document.getElementById("payment-option-1") ;
 let pay_with_payment_option_1_form = document.getElementById("pay-with-payment-option-1-form") ;
 //if (payment_option_1) {
-//payment_option_1.checked = "checked" ;
+payment_option_1.checked = "checked" ;
 //}
 //if (pay_with_payment_option_1_form) {
 //pay_with_payment_option_1_form.style.display = "block" ;
