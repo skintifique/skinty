4428a409ab056315ff6a87b09f0c252467ad4ce2
skintifique <78672198+skintifique@users.noreply.github.com>
2024-04-11 23:54:34 +0200

    Update skinty_preprod.js

--- skinty_preprod.js
+++ skinty_preprod.js
@@ -102,7 +102,7 @@ document.getElementById("ModalBtnFloat").style.display = "none";
 if ((pageUrl.includes("/en/order")) || (pageUrl.includes("/fr/commande")))  {
 let payment_option_1 = document.getElementById("payment-option-1") ;
 let pay_with_payment_option_1_form = document.getElementById("pay-with-payment-option-1-form") ;
-// payment_option_1.checked = "checked" ;
+payment_option_1.checked = true ;
 pay_with_payment_option_1_form.style.display = "block" ;
 }
 // END SCRIPT TO MAKE FIRST PAYMENT OPTION VISIBLE AND CORRESPONDING RADIO BTN CKECKED ON PAYMENT PAGE
