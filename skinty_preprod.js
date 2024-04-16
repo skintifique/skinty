49fdb6a4e0dc2451394fad0a7645f7cb6124d198
skintifique <78672198+skintifique@users.noreply.github.com>
2024-04-16 14:11:30 +0200

    Update skinty_preprod.js

--- skinty_preprod.js
+++ skinty_preprod.js
@@ -99,14 +99,14 @@ document.getElementById("ModalBtnFloat").style.display = "none";
 // END SCRIPT TO ADD BANNER TEXT BEFORE PRICE DISCOUNT TABLE ON PRODUCT PAGE 
 
 //  SCRIPT TO MAKE FIRST PAYMENT OPTION VISIBLE AND CORRESPONDING RADIO BTN CKECKED ON PAYMENT PAGE
-if ((pageUrl.includes("/en/order")) || (pageUrl.includes("/fr/commande")))  {
-let payment_option_1 = document.getElementById("payment-option-1") ;
-let pay_with_payment_option_1_form = document.getElementById("pay-with-payment-option-1-form") ;
+// if ((pageUrl.includes("/en/order")) || (pageUrl.includes("/fr/commande")))  {
+// let payment_option_1 = document.getElementById("payment-option-1") ;
+// let pay_with_payment_option_1_form = document.getElementById("pay-with-payment-option-1-form") ;
 // payment_option_1.checked = true ;
-if (pay_with_payment_option_1_form) {
-	pay_with_payment_option_1_form.style.display = "block" ;	
-	} 
-}
+// if (pay_with_payment_option_1_form) {
+// 	pay_with_payment_option_1_form.style.display = "block" ;	
+// 	} 
+// }
 // END SCRIPT TO MAKE FIRST PAYMENT OPTION VISIBLE AND CORRESPONDING RADIO BTN CKECKED ON PAYMENT PAGE
 
 
